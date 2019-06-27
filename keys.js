1816a83db85c2d348a7fde221865ff7a51bd7419
rcbnyc <16548379+ryancbrown@users.noreply.github.com>
2019-06-26 23:36:28 -0400

    remove spotify keys from keys.js and move to .env. roll secret key

--- keys.js
+++ keys.js
@@ -1,8 +1,5 @@
 console.log('this is loaded');
 
-SPOTIFY_ID = '255221e3beb54644b406a6cced455932'
-SPOTIFY_SECRET = 'c771149e911942aeb62a75607468fd0b'
-
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
