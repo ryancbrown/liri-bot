57b0583e8d3a65558e5b84e7860ec8fee28dd92a
rcbnyc <16548379+ryancbrown@users.noreply.github.com>
2019-07-01 02:36:10 -0400

    add log.txt and  logic

--- function.js
+++ function.js
@@ -17,6 +17,26 @@ var bands = function(input) {
         method: 'get',
         url: 'https://rest.bandsintown.com/artists/' + input + '/events?app_id=codingbootcamp',
     }).then(function (response) {
+        // Log search
+        if (on) {
+            // Add to log.txt
+            fs.appendFile('log.txt','concert-this ' + input + '\n', function(err) { 
+                console.log(err)
+            }) 
+
+            // Add to random.txt
+            fs.appendFile('random.txt', 'concert-this,' + input + ',', function(err) {
+                if (err) { 
+                    console.log('Error saving upcoming concerts.')
+                } 
+            });
+        } else {
+            fs.appendFile('log.txt','do-what-it-says\n', function(err) { 
+                    console.log(err)
+            }) 
+            on = true
+        }
+
        // Capitalize first letter of each word for input formatting
         function capitalize() { 
             str = input.split(' ');
@@ -29,27 +49,28 @@ var bands = function(input) {
         } 
 
         if (response.data.length > 0) {
-            console.log(response.data)
-            console.log('\n\nUpcoming ' + capitalize(input) + ' Shows \n--------------');
-            for (var i = 0; i < response.data.length; i++) {
-                var city;
+            var city;
 
-                // If non-US city then show country instead of state
+            // If non-US city then show country instead of state
+
+            console.log('\n\nUpcoming ' + capitalize(input) + ' Shows \n--------------');
+
+            for (var i = 0; i < response.data.length; i++) {
                 if (response.data[i].venue.region !== '') { 
-                    city = response.data[i].venue.city + ', ' + response.data[i].venue.region
+                    city = response.data[i].venue.city + ', '
                 } else { 
                     city = response.data[i].venue.city + ', ' + response.data[i].venue.country
                 } 
-
+    
                 console.log('Venue: ' + response.data[i].venue.name + '\nCity: ' + city + response.data[i].venue.region + '\nDate: ' + moment(response.data[i].datetime).format('YYYY-MM-DD HH:mm'));
                 console.log('--------------');
             }
-            console.log('\n\n')
+            console.log('\n')
         } else { 
-            console.log('There are no upcoming concerts for ' + capitalize(input) + '. Try another artist.');
+            console.log('\nThere are no upcoming concerts for ' + capitalize(input) + '. Try another artist.\n');
         }
     }).catch(function (error) {
-        console.log('Error: Artist not found. Try another search.\n\n');
+        console.log('\nError: Artist not found. Try another search.\n')
     });
 }
 
@@ -59,6 +80,17 @@ var song = function(input) {
         type: 'track', 
         query: input 
     }).then(function(response) {
+        if(on) {
+            // Log search
+            fs.appendFile('log.txt','spotify-this-song ' + input + '\n', function(err) { 
+                console.log(err)
+            }) 
+        } else { 
+            // Log search
+            fs.appendFile('log.txt','do-what-it-says\n', function(err) { 
+                console.log(err)
+            }) 
+        }
         // Store all songs returned from api call so future api calls do not need to be made
         // Prevent duplication
         if (songs.length === 0) {
@@ -66,11 +98,10 @@ var song = function(input) {
                 songs.push({'artist':response.tracks.items[i].album.artists[0].name, 'album':response.tracks.items[i].album.name,'song':response.tracks.items[i].name,'preview':response.tracks.items[i].preview_url})
             }
         } 
-        // Prompt the user to confirmn whether or not their song was found
+        // Prompt the user to confirm whether or not their song was found
         if (on) { 
             console.log('\n\nSong ' + (count + 1) + '/' + songs.length + '\nArtist: ' + songs[0].artist + '\nAlbum: ' + songs[0].album + '\nSong: ' + songs[0].song + '\nPreview: ' + songs[0].preview + '\n\n')
             prompt()
-            on = true
         } else { 
             console.log('\n\nArtist: ' + songs[0].artist + '\nAlbum: ' + songs[0].album + '\nSong: ' + songs[0].song + '\nPreview: ' + songs[0].preview + '\n\n')
         }
@@ -85,34 +116,65 @@ var omdb = function(input) {
         method: 'get',
         url: 'http://www.omdbapi.com/?apikey=trilogy&t=' + input,
     }).then(function (response) {
-        console.log('--------------')
-        console.log('Title: ' + response.data.Title); // title
-        console.log('Year: ' + response.data.Released); // year
-        console.log('IMDB: ' + response.data.Ratings[0].Value); // imdb
-        console.log('Rotten Tomatoes: ' + response.data.Ratings[1].Value); // rotten tomatoes
-        console.log('Country: ' + response.data.Country); // country
-        console.log('Language: ' + response.data.Language); // language
-        console.log('Plot: ' + response.data.Plot); // plot
-        console.log('Actors: ' + response.data.Actors); // actors      
+        if (on) { 
+            // Log search
+            fs.appendFile('log.txt','movie-this ' + input + '\n', function(err) { 
+                if (err) { 
+                }
+            }) 
+
+            // Add to random.txt
+            fs.appendFile('random.txt', 'movie-this,' + input + ',', function(err) {
+                if (err) { 
+                    console.log('\nError saving movie.\n')
+                }  
+            });
+        }  else {
+            fs.appendFile('log.txt','do-what-it-says\n', function(err) { 
+                    console.log(err)
+            }) 
+            on = true
+        }
+
+        console.log('\n\nTitle: ' + response.data.Title + '\nYear: ' + response.data.Released + '\nIMDB: ' + response.data.Ratings[0].Value + '\nRotten Tomatoes: ' + response.data.Ratings[1].Value + '\nCountry: ' + response.data.Country  + '\nLanguage: ' + response.data.Language + '\nPlot: ' + response.data.Plot + '\nActors: ' + response.data.Actors + '\n\n');   
     }).catch(function (error) {
-        console.log(error);
+        if (error) { 
+            console.log('\nMovie not found. Please search again\n');
+        } 
     });
 }
 
 var random = function() { 
+    // Read random.txt
     fs.readFile('random.txt', "utf8", function(error, data){
         var randomObj = Math.round(Math.random() * ((data.split(',').length) - 1))
-
-        // If spotify-this-song grab the next item in array as it will always be a song
-        if (data.split(',')[randomObj] === 'spotify-this-song') {
-            console.log('\n\nSearching for ' + data.split(',')[randomObj + 1])
-            on = false
-            song(data.split(',')[randomObj + 1])
+        var obj = data.split(',')[randomObj]
+        var request;
+        var search;
+        
+        // Assuming that the band, movie, and song passed in by the user live in even numbered positions on the array
+        // Determine if the random number chosen is even or odd and set request equal to the user request—e.g. `movie-this`
+        if (randomObj % 2 !== 0 || randomObj === 1) {
+            request = data.split(',')[randomObj - 1]
+            search = data.split(',')[randomObj]
         } else { 
-            console.log('\n\nSearching for ' + data.split(',')[randomObj])
-            // Flip the save song option off if choosing random song as it has already been saved
+            request = data.split(',')[randomObj]
+            search = data.split(',')[randomObj + 1] 
+        }        
+
+        // Set up the logic to decide on how to log data
+        if (request === 'spotify-this-song') {
+            console.log('\n\nSearching for a song...')
             on = false
-            song(data.split(',')[randomObj])
+            song(search)
+        } else if (request === 'movie-this') { 
+            console.log('\n\nSearching for a movie...')
+            on = false
+            omdb(search)
+        } else if (request === 'concert-this') { 
+            console.log('\n\nSearching for a movie...')
+            on = false
+            bands(search)
         }
     });
 
@@ -142,7 +204,7 @@ function prompt() {
                 } else {
                     console.log('Song saved.\n');
                 }
-              });
+            });
         } else { 
             if (count + 1 < songs.length) {
                 // Grab next song from songs array and print
